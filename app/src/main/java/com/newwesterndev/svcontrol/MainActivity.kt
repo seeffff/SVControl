07d7980485dab674941cd0d867af9ce2c49de56f
seeffff <jdephil@gmail.com>
2017-09-06 22:30:43 -0700

    Added button to start and stop an empty service

--- app/src/main/java/com/newwesterndev/svcontrol/MainActivity.kt
+++ app/src/main/java/com/newwesterndev/svcontrol/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.newwesterndev.svcontrol
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import butterknife.bindView
 
 class MainActivity : AppCompatActivity() {
+
+    private val mStartButton: Button by bindView(R.id.start_button)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val i = Intent(this, SVService::class.java)
+
+        mStartButton.setOnClickListener { _ ->
+            if(mStartButton.text == getString(R.string.start_button)) {
+                startService(i)
+                showToast("Service has been started")
+                mStartButton.setText(getString(R.string.stop_button))
+            }
+            else {
+                stopService(i)
+                showToast("Service has been stopped")
+                mStartButton.setText(getString(R.string.start_button))
+            }
+        }
+    }
+
+    private fun showToast(message: String){
+        val t: Toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+        t.show()
     }
 }
---
+++ app/src/main/java/com/newwesterndev/svcontrol/SVService.kt
@@ -0,0 +1,13 @@
+package com.newwesterndev.svcontrol
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class SVService : Service() {
+
+    override fun onBind(p0: Intent?): IBinder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
