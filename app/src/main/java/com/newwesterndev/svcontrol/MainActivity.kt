aadcb4d2153000131016634c321c500291862963
seeffff <jdephil@gmail.com>
2017-09-23 09:43:32 -0700

    Handled device orientation issues

--- app/src/main/java/com/newwesterndev/svcontrol/MainActivity.kt
+++ app/src/main/java/com/newwesterndev/svcontrol/MainActivity.kt
@@ -19,57 +19,83 @@ class MainActivity : AppCompatActivity() {
     private val mLowSpeedText: TextView by bindView(R.id.low_mph_text)
     private val mLowVolumeText: TextView by bindView(R.id.low_volume_text)
 
-    private val MPS_TO_MPH = 2.23694
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 10)
 
         init()
-
         val i = Intent(this, SVService::class.java)
 
         mStartButton.setOnClickListener { _ ->
             if (mStartButton.text == getString(R.string.start_button)) {
-                i.putExtra("lowspeed", mLowSpeedSeek.progress)
-                i.putExtra("lowvolume", mLowVolumeSeek.progress)
-                startService(i)
-                enableSeekBars(false)
-                showToast("Service has been started")
-                mStartButton.setText(getString(R.string.stop_button))
+                activateService(i)
             } else {
-                stopService(i)
-                enableSeekBars(true)
-                showToast("Service has been stopped")
-                mStartButton.setText(getString(R.string.start_button))
+                deactivateService(i)
             }
         }
 
-            mLowSpeedSeek.onProgressChanged { progress, _ ->
-                var conversion = ((progress * MPS_TO_MPH).toFloat()).toInt()
-                var progressString: String = conversion.toString() + " " + resources.getString(R.string.speed_text)
-                mLowSpeedText.text = progressString
-            }
-            mLowVolumeSeek.onProgressChanged { progress, _ ->
-                var progressString: String = progress.toString() + " " + resources.getString(R.string.percent_text)
-                mLowVolumeText.text = progressString
-            }
+        mLowSpeedSeek.onProgressChanged { progress, _ ->
+            var progressString: String = progress.toString() + " " + resources.getString(R.string.speed_text)
+            mLowSpeedText.text = progressString
+        }
+        mLowVolumeSeek.onProgressChanged { progress, _ ->
+            var progressString: String = progress.toString() + " " + resources.getString(R.string.percent_text)
+            mLowVolumeText.text = progressString
+        }
 
+        if(savedInstanceState != null){
+            var isRunning = savedInstanceState.getBoolean("running")
+            if(isRunning){
+                enableServiceActiveUI()
+            }
+        }
     }
 
-    private fun init(){
-        mLowSpeedSeek.max = 30
+    fun activateService(i: Intent) {
+        i.putExtra("lowspeed", mLowSpeedSeek.progress)
+        i.putExtra("lowvolume", mLowVolumeSeek.progress)
+        startService(i)
+        mIsServiceRunning = true
+        enableSeekBars(false)
+        showToast("Service has been started")
+        mStartButton.setText(getString(R.string.stop_button))
+    }
+
+    fun deactivateService(i: Intent) {
+        stopService(i)
+        mIsServiceRunning = false
+        enableSeekBars(true)
+        showToast("Service has been stopped")
+        mStartButton.setText(getString(R.string.start_button))
+    }
+
+    fun enableServiceActiveUI(){
+        enableSeekBars(false)
+        mStartButton.setText(getString(R.string.stop_button))
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        outState!!.putBoolean("running", mIsServiceRunning)
+        super.onSaveInstanceState(outState)
+    }
+
+    private fun init() {
+        mLowSpeedSeek.max = 50
         mLowVolumeSeek.max = 100
     }
 
-    private fun enableSeekBars(bool: Boolean){
+    private fun enableSeekBars(bool: Boolean) {
         mLowSpeedSeek.isEnabled = bool
         mLowVolumeSeek.isEnabled = bool
     }
 
-    private fun showToast(message: String){
+    private fun showToast(message: String) {
         val t: Toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
         t.show()
+    }
+
+    companion object {
+        var mIsServiceRunning = false
     }
 }
