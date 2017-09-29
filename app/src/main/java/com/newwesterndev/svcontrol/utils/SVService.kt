a5b9c2ccc4443f7a8c7e3a4c57da05feb642fd09
seeffff <jdephil@gmail.com>
2017-09-29 11:29:13 -0700

    Will now show notification when service is running

--- app/src/main/java/com/newwesterndev/svcontrol/utils/SVService.kt
+++ app/src/main/java/com/newwesterndev/svcontrol/utils/SVService.kt
@@ -3,11 +3,14 @@ package com.newwesterndev.svcontrol.utils
 import android.app.*
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.location.Location
 import android.location.LocationManager
 import android.media.AudioManager
 import android.os.Bundle
 import android.util.Log
+import com.newwesterndev.svcontrol.MainActivity
+import com.newwesterndev.svcontrol.R
 
 class SVService : Service() {
     private var locationManager: LocationManager? = null
@@ -26,6 +29,8 @@ class SVService : Service() {
         mVolController = VolController(this, lowSpeed, lowVolume)
         val mUtility = Utility(this)
         mUtility.saveHighStream(mAudioManger.getStreamVolume(AudioManager.STREAM_MUSIC))
+
+        showNotification()
 
         return START_STICKY
     }
@@ -52,7 +57,7 @@ class SVService : Service() {
     }
 
     override fun onDestroy() {
-        super.onDestroy()
+        mNotificationManager!!.cancel(1)
         if (locationManager != null)
             for (i in 0..locationListeners.size) {
                 try {
@@ -61,6 +66,26 @@ class SVService : Service() {
                     Log.w(TAG, "Failed to remove location listeners")
                 }
             }
+        super.onDestroy()
+    }
+
+    private fun showNotification(){
+        mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val pendingIntent = PendingIntent.getActivity(this, 0, Intent(this, MainActivity::class.java), 0)
+        val builder = Notification.Builder(this)
+
+        builder.setAutoCancel(false)
+        builder.setContentTitle(resources.getString(R.string.app_name))
+        builder.setContentText(resources.getString(R.string.notif_text))
+        builder.setSmallIcon(R.drawable.ic_volume_up_white_18dp)
+        builder.setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
+        builder.setContentIntent(pendingIntent)
+        builder.setOngoing(true)
+        builder.build()
+
+        var notification = builder.notification
+        notification.flags = Notification.FLAG_ONGOING_EVENT
+        mNotificationManager!!.notify(1, notification)
     }
 
     companion object {
@@ -77,6 +102,7 @@ class SVService : Service() {
 
         var mVolController: VolController? = null
         var mSpeedUnits: String? = null
+        var mNotificationManager: NotificationManager? = null
 
         class LTRLocationListener(provider: String) : android.location.LocationListener {
 
