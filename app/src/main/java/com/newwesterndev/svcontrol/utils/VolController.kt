357580ae413c4ca40e2fee0013c1bcce91b371c0
seeffff <jdephil@gmail.com>
2017-09-29 11:07:36 -0700

    Helped patch issue of random volume drops at a false 0 speed.

--- app/src/main/java/com/newwesterndev/svcontrol/utils/VolController.kt
+++ app/src/main/java/com/newwesterndev/svcontrol/utils/VolController.kt
@@ -9,6 +9,7 @@ class VolController(c: Context, ls: Int, lv: Int) {
     private val mLowSpeed = ls
     private var mLowStreamVolume = lv
     private var mUtility = Utility(c)
+    private var mLastSpeed = 0F
 
     private var mAudioManager: AudioManager =
             c.getSystemService(Context.AUDIO_SERVICE) as AudioManager
@@ -18,17 +19,24 @@ class VolController(c: Context, ls: Int, lv: Int) {
         e("Low stream vol " + mLowStreamVolume.toString())
         e("Current vol " + mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC).toString())
         e("Current speed " + speed.toString())
+        e("Current last speed " + mLastSpeed.toString())
         e("Speed to drop vol " + mLowSpeed.toString())
 
         val currentStreamVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
 
-        if (mAudioManager.isMusicActive) {
-            if (speed <= mLowSpeed && currentStreamVolume > mLowStreamVolume){
-                mUtility.saveHighStream(currentStreamVolume)
-                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, mLowStreamVolume, 0)
-            } else if (speed > mLowSpeed && currentStreamVolume == mLowStreamVolume) {
-                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, mUtility.getHighStream(), 0)
+        if (speed == 0F && mLastSpeed != 0F) {
+
+        } else {
+            if (mAudioManager.isMusicActive) {
+                if (speed <= mLowSpeed && currentStreamVolume > mLowStreamVolume) {
+                    mUtility.saveHighStream(currentStreamVolume)
+                    mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, mLowStreamVolume, 0)
+                } else if (speed > mLowSpeed && currentStreamVolume == mLowStreamVolume) {
+                    mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, mUtility.getHighStream(), 0)
+                }
             }
         }
+
+        mLastSpeed = speed
     }
 }
